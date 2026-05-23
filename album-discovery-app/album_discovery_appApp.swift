862d176feda518ccff80a149230d868e0f37d559
Shimpei Nakai <130976305+nki1121@users.noreply.github.com>
2026-05-23 18:44:30 +0900

    プロジェクトの立ち上げ

---
+++ album-discovery-app/album_discovery_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  album_discovery_appApp.swift
+//  album-discovery-app
+//
+//  Created by nakaishimpei on 2026/05/23.
+//
+
+import SwiftUI
+
+@main
+struct album_discovery_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ album-discovery-appTests/album_discovery_appTests.swift
@@ -0,0 +1,17 @@
+//
+//  album_discovery_appTests.swift
+//  album-discovery-appTests
+//
+//  Created by nakaishimpei on 2026/05/23.
+//
+
+import Testing
+@testable import album_discovery_app
+
+struct album_discovery_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
