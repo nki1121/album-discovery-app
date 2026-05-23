ab58e3c752b85720ff43a0f7fb6f484fbf98eafc
Shimpei Nakai <130976305+nki1121@users.noreply.github.com>
2026-05-23 20:36:55 +0900

    MockTrackService.swiftの実装が完了した。

--- album-discovery-app/Services/MockTrackService.swift
+++ album-discovery-app/Services/MockTrackService.swift
@@ -1,18 +1,26 @@
 import Foundation
 
+// このServiceで発生しうるエラー
+enum MockTrackServiceError: Error {
+    case fileNotFound
+}
+
 // アプリバンドルに同梱された mock_tracks.json を読み込んで [Track] を返す Service。
-//
-// 今は async/await のシグネチャだけ用意してあるが、中身は同期処理でも問題ない。
-// 将来 SpotifyTrackService に差し替えるときに自然に async が活きてくる。
 struct MockTrackService {
 
     func fetchTracks() async throws -> [Track] {
-        // TODO: Bundle.main.url(forResource: "mock_tracks", withExtension: "json") で URL を取得
-        // TODO: 取得できない場合はエラーを throw する (独自エラー型 or NSError)
-        // TODO: Data(contentsOf: url) でデータを読み込む
-        // TODO: JSONDecoder().decode([Track].self, from: data) でデコード
-        // TODO: デコード結果を return する
+        // ① バンドル内の mock_tracks.json の場所を取得
+        guard let url = Bundle.main.url(forResource: "mock_tracks", withExtension: "json") else {
+            throw MockTrackServiceError.fileNotFound
+        }
 
-        return []
+        // ② URL からデータを読み込む
+        let data = try Data(contentsOf: url)
+
+        // ③ JSON をデコードして [Track] にする
+        let decoder = JSONDecoder()
+        let tracks = try decoder.decode([Track].self, from: data)
+
+        return tracks
     }
 }
